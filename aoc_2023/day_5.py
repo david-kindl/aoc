5e99016fbc0cfd00dffc8bd7b156d8c2f3c68ab1
david-kindl <davidkindl92@gmail.com>
2023-12-04 20:17:32 +0100

    Created init file for day 5

---
+++ aoc_2023/day_5.py
@@ -0,0 +1,14 @@
+from solution import Solution
+
+
+class Solve(Solution):
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+
+    @property
+    def part_1(self) -> any:
+        pass
+
+    @property
+    def part_2(self):
+        pass
